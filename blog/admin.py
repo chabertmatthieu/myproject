2c2d404870622ca6ecc67fcc7d0256a7b1c3ca44
Matthieu Chabert <matthieuchabert@MacBook-Air-de-Matthieu.local>
2017-02-08 16:13:21 +0100

    Chapitre 5 : l'administration

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.utils.text import Truncator
+from .models import Article, Categorie
 
-# Register your models here.
+
+
+
+
+class ArticleAdmin(admin.ModelAdmin):
+	list_display = ('titre', 'auteur', 'date', 'apercu_contenu')
+	list_filter = ('auteur', 'categorie',)
+	ordering = ('date',)
+	search_fields = ('titre', 'contenu')
+	date_hierarchy = 'date'
+	prepopulated_fields = {'slug': ('titre',),}
+
+	fieldsets = (
+		('Général', {
+			'fields' : ('titre', 'slug', 'auteur', 'categorie')
+		}),
+		('Contenu de l\'article', {
+			'description' : ('Le formulaire accepte les balises HTML.'),
+			'fields' : ('contenu',)
+
+			}),
+		)
+
+	def apercu_contenu(self, article):
+		return Truncator(article.contenu).chars(40, truncate= '...')
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Categorie)
